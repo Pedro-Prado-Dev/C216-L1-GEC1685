ed77b78e3d95625fdcd3fd539dc8a4b9e6045d62
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-04-06 11:10:59 -0300

    :zap: organization: insert pratica in aula

--- pratica-0/src/index.js
+++
@@ -1,8 +0,0 @@
-let nome = "[Pedrão] - [GEC1685]";
-function saudacaoPersonalizada(nome) {
-    console.log("Olá, " + nome + "!");
-}
-saudacaoPersonalizada(nome);
-
-let modulo = require('./module.js');
-modulo.saudacao();
